1eb79f19b20f9b8daecf451b0727ce783dcffa92
William Hutcheon <william.hutcheon@cmenc.com>
2025-09-29 12:15:46 -0400

    sound works on iphone

--- Platforms/iOS/BuzzAndDing.cs
+++ Platforms/iOS/BuzzAndDing.cs
@@ -9,33 +9,12 @@ namespace PUBTransfer.Platforms.iOS
     public class BuzzAndDing
     {
         private AVAudioPlayer player;
-        //public BuzzAndDing()
-        //{
-        //    // Load the sound file from app bundle
-        //    //var url = NSBundle.MainBundle.GetUrlForResource("ding", "mp3");
-        //    var url = NSBundle.MainBundle.GetUrlForResource("ding", "wav");
-        //    if (url != null)
-        //    {
-        //        player = AVAudioPlayer.FromUrl(url);
-        //        player.PrepareToPlay();
-        //    }
-        //}
-        //public void Ding()
-        //{
-        //    // Play custom sound
-        //    player?.Play();
-        //    // Vibrate / Haptic feedback
-        //    UIDevice.CurrentDevice.PlayInputClick(); // light haptic
-        //    SystemSound.Vibrate.PlaySystemSound();   // default vibration
-        //}
-
         public BuzzAndDing()
         {
             // Configure audio session
             var session = AVAudioSession.SharedInstance();
             session.SetCategory(AVAudioSessionCategory.Playback, AVAudioSessionCategoryOptions.MixWithOthers);
             session.SetActive(true);
-
             // Load sound
             var url = NSBundle.MainBundle.GetUrlForResource("ding", "wav");
             if (url != null)
@@ -44,14 +23,12 @@ namespace PUBTransfer.Platforms.iOS
                 player.PrepareToPlay();
             }
         }
-
         public void Ding()
         {
             player?.Play();
             UIDevice.CurrentDevice.PlayInputClick(); // light haptic
             SystemSound.Vibrate.PlaySystemSound();   // default vibration
         }
-
         public void Release()
         {
             player?.Dispose();
